d27dd881fb6a5f3116dd5ae8d134e05c7c1336f0
Alexander Kurakin <A1esandr@users.noreply.github.com>
2020-09-23 00:34:00 +0500

    Update solution.go

--- twochannels/solution.go
+++ twochannels/solution.go
@@ -1,65 +1,79 @@
 package main
 
-import (
-	"fmt"
-)
-
-func gen(nums ...int) <-chan int {
-	out := make(chan int, len(nums))
-	go func() {
-		for _, n := range nums {
-			select {
-			case out <- n:
-				continue
-			}
-		}
-		close(out)
-	}()
-	return out
-}
-
-func main() {
-
-	c1 := gen(1, 2, 3)
-	c2 := gen(1, 2, 3)
-	out := make(chan int)
-
-	Merge2Channels(ff, c1, c2, out, 3)
-
-	for i := 0; i < 3; i++ {
-		select {
-		case n := <-out:
-			fmt.Println(n)
-		}
-	}
-}
-
-func ff(a int) int {
-	return a * a
-}
-
 func Merge2Channels(f func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+
+	if n == 0 {
+		return
+	}
+
+	buf1 := make(chan int, n)
+	buf2 := make(chan int, n)
+	closebuf1 := make(chan int, n)
+	closebuf2 := make(chan int, n)
+
+	go load(in1, buf1, n)
+	go load(in2, buf2, n)
+
+	out1 := make(chan int, n)
+	out2 := make(chan int, n)
+	closeout := make(chan int, n)
+
 	for i := 0; i < n; i++ {
-		out1 := exec(f, in1)
-		out2 := exec(f, in2)
-		sum(out1, out2, out)
+		go exec(f, buf1, out1, closebuf1)
+		go exec(f, buf2, out2, closebuf2)
+		go sum(out1, out2, out, closeout)
+	}
+
+	go closechan(closebuf1, buf1, n)
+	go closechan(closebuf2, buf2, n)
+	go closechanout(closeout, out1, out2, n)
+}
+
+func load(in1 <-chan int, buf chan int, n int) {
+	for i := 0; i < n; i++ {
+		select {
+		case v := <-in1:
+			buf <- v
+		}
 	}
 }
 
-func exec(f func(int) int, in1 <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		n0 := <-in1
-		out <- f(n0)
-		close(out)
-	}()
-	return out
+func exec(f func(int) int, in <-chan int, out chan int, closebuf chan int) {
+	n0 := <-in
+	select {
+	case out <- f(n0):
+		closebuf <- 1
+		return
+	}
 }
 
-func sum(in1 <-chan int, in2 <-chan int, out chan<- int) {
-	go func() {
-		n1 := <-in1
-		n2 := <-in2
-		out <- n1 + n2
-	}()
+func sum(in1 <-chan int, in2 <-chan int, out chan<- int, closebuf chan int) {
+	n1 := <-in1
+	n2 := <-in2
+	select {
+	case out <- n1 + n2:
+		closebuf <- 1
+		return
+	}
+}
+
+func closechan(closebuf chan int, buf chan int, n int) {
+	for i := 0; i < n; i++ {
+		select {
+		case <-closebuf:
+		}
+	}
+	close(closebuf)
+	close(buf)
+}
+
+func closechanout(closeout chan int, out1 chan int, out2 chan int, n int) {
+	for i := 0; i < n; i++ {
+		select {
+		case <-closeout:
+		}
+	}
+	close(closeout)
+	close(out1)
+	close(out2)
 }
